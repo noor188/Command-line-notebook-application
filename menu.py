3be88b76392c05d6b876f6f2a4fb862385b7581e
Noor Mamlook <noormmlk@gmail.com>
2023-10-11 23:48:00 -0600

    created menu class

---
+++ menu.py
@@ -0,0 +1,44 @@
+from note import Note
+from notebook import Notebook
+
+class Menu:
+    '''Display a menu and respond to choices when run.'''
+    def __init__(self):
+        self.notebook = Notebook()
+        self.choices = {
+            '1': self.show_notes,
+            '2': self.search_notes,
+            '3': self.add_note,
+            '4': self.modify_note,
+            '5': self.quit
+        }
+    
+    def displayMenu(self):
+        print("""
+                Notebook Menu
+              
+              1. Show all Notes
+              2. Search Notes
+              3. Add Note
+              4. Modify Note
+              5. Quit
+                """)
+    
+    def run(self):
+        '''Display the menu and respond to choices.'''
+        while True:
+            self.displayMenu()
+            choice = input('Enter an option: ')
+            action = self.choices.get(choice)
+            if action :
+                action()
+            else:
+                print('{0} is not a valid choice'.format(choice))
+
+    def show_notes(self, notes= None):
+        if not notes:
+            notes = self.notebook.notes
+        for note in notes:
+            print('{0}: {1}\n{2}'.format(note.id, note.tags, note.memo))    
+    
+
